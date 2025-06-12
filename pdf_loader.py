1e02f82ad8dc8ce993047982536ab52522161f7d
Tushar Singh <tushar80rt@gmail.com>
2025-06-12 09:36:44 +0530

    pdf_loader.py

--- pdf_loader.py
+++
@@ -1,8 +0,0 @@
-from PyPDF2 import PdfReader
-
-def load_pdf_chunks(path, chunk_size=500):
-    reader = PdfReader(path)
-    full_text = ""
-    for page in reader.pages:
-        full_text += page.extract_text() + "\n"
-    return [full_text[i:i+chunk_size] for i in range(0, len(full_text), chunk_size)]
